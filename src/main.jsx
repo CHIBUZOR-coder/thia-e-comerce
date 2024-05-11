37a87882ff84521badca3ca2b61c7a22ef6a9f5b
Chibuzor <chibuzormekalam@gmail.com>
2024-05-11 21:21:52 +0100

    updated router path to thia-e-comerce in main.jxs

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/",
+        path: "/thia-e-comerce/",
         element:<Home/> ,
       },
     ],
