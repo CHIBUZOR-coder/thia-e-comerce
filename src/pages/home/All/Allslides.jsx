87299a789d3249e5ad775ade9305a11f37a04f26
Chibuzor <chibuzormekalam@gmail.com>
2024-05-14 09:28:07 +0100

    add box shadow, scaling  and hover effect  to carocel, and carocel btns container

--- src/pages/home/All/Allslides.jsx
+++ src/pages/home/All/Allslides.jsx
@@ -54,59 +54,61 @@ const Allslides = () => {
   }
 
   return (
-    <div className='w-full md:w-1/2 overflow-hidden md:m-0 mt-3 md:py-16'>
-      <h1 className='text-3xl md:py-6 font-sans flex justify-center font-semibold'>
-      Featured
+    <div className="w-full md:w-1/2 px-0 md:px-2 overflow-hidden md:m-0 mt-3 md:py-16">
+      <h1 className="text-3xl md:py-6 font-sans flex justify-center font-semibold">
+        Featured
       </h1>
-      <div
-        className='relative flex w-full transition ease-in-out duration-1000'
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-      >
-        {slides.map((slide, index) => (
-          <div
-            key={index}
-            className={`w-full carocel transition ease-in-out duration-1000 bg-slate-400 ${
-              index === currentSlide ? '' : 'hidden'
-            } flex justify-center  `}
+      <div className="carsel w-full carsel  ">
+        <div
+          className="relative   flex w-full transition ease-in-out duration-1000"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          {slides.map((slide, index) => (
+            <div
+              key={index}
+              className={`w-full carocel transition ease-in-out duration-1000 bg-slate-400 ${
+                index === currentSlide ? "" : "hidden"
+              } flex justify-center  `}
+            >
+              {React.cloneElement(slide, {
+                style: {
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover", // Ensure the image covers the container
+                },
+              })}
+            </div>
+          ))}
+          {/* Manual navigation buttons */}
+          <button
+            onClick={prevSlide}
+            className="absolute top-1/2 left-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-l"
           >
-            {React.cloneElement(slide, {
-              style: {
-                width: '100%',
-                height: '100%',
-                objectFit: 'cover' // Ensure the image covers the container
-              }
-            })}
-          </div>
-        ))}
-        {/* Manual navigation buttons */}
-        <button
-          onClick={prevSlide}
-          className='absolute top-1/2 left-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-l'
-        >
-          Prev
-        </button>
-        <button
-          onClick={nextSlide}
-          className='absolute top-1/2 right-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-r'
-        >
-          Next
-        </button>
+            Prev
+          </button>
+          <button
+            onClick={nextSlide}
+            className="absolute top-1/2 right-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-r"
+          >
+            Next
+          </button>
+        </div>
       </div>
       {/* Navigation dots */}
-      <div className='flex justify-center mt-4'>
+      <div className="flex justify-center mt-4">
         {slides.map((_, index) => (
           <span
             key={index}
             className={`mx-1 h-3 w-3 rounded-full cursor-pointer ${
-              index === currentSlide ? 'bg-gray-900' : 'bg-gray-400'
+              index === currentSlide ? "bg-gray-900" : "bg-gray-400"
             }`}
             onClick={() => setCurrentSlide(index)}
           />
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default Allslides
