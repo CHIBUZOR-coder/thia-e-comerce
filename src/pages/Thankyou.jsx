4c330ba721c0411538c54ef9be0b3bf15ff300cd
Chibuzor <chibuzormekalam@gmail.com>
2025-03-11 16:00:40 +0100

    fixed verification of payment

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -4,12 +4,22 @@ import { useNavigate } from "react-router-dom";
 
 const Thankyou = () => {
   const [searchParams] = useSearchParams();
+  const UserInfo = JSON.parse(localStorage.getItem("userInfo")) || false;
+  let user;
+  useEffect(() => {
+    console.log("Thankyou:", UserInfo?.email);
+    if (UserInfo && UserInfo.email) {
+      user = UserInfo?.email;
+    }
+  });
   const [transactionId, setTransaction] = useState(
     searchParams.get("transaction_id")
   );
+  const [OrderId, setOrderId] = useState(searchParams.get("orderId"));
   const [isLoading, setIsLoading] = useState(true);
   const [status, setStatus] = useState("");
-  const orderId = localStorage.getItem("orderId");
+  const [Products, setProducts] = useState([]);
+  const [Bill, setBill] = useState(null);
 
   const [emojis, setEmojis] = useState([]); // Store emoji objects
   const navigate = useNavigate();
@@ -18,53 +28,48 @@ const Thankyou = () => {
     console.log("status:", status);
   }, [status]);
 
-  const createReciept = async (transaction_id, orderId) => {
+  const createReciept = async (transaction_id, orderId, email) => {
     try {
-      const response = await fetch(
-        "https://streambackend-nbbc.onrender.com/verify",
-        {
-          method: "POST",
+      const response = await fetch("http://localhost:5000/verify_payment", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
 
-          headers: {
-            "Content-Type": "application/json",
-          },
-
-          body: JSON.stringify({ transaction_id, orderId, email }),
-        }
-      );
+        body: JSON.stringify({ transaction_id, orderId, email }),
+      });
       const data = await response.json();
       if (response.ok) {
         console.log(data);
-        setResult(Alert(true, data.message) || "Order successful");
+
         localStorage.setItem("Recipt", data.data);
         setIsLoading(false);
         localStorage.removeItem("orderId");
-        setStatus(data.data.status);
+        setStatus(data?.data?.status);
+        setBill(data?.data?.bill);
+        setProducts(data?.data?.products);
         Autentification();
         setTimeout(() => {
           navigate("/");
         }, 4000);
       } else {
-        setResult(Alert(false, data.message) || "Order failed");
+        console.log(data || "Something went wrong");
+        console.log("status", data.data.status);
       }
     } catch (error) {
       console.log(error);
-    } finally {
-      setTimeout(() => {
-        setResult(null);
-      }, 3000);
     }
   };
 
   useEffect(() => {
     if (transactionId) {
-      createReciept(transactionId, orderId);
+      createReciept(transactionId, OrderId, user);
     }
   }, [transactionId]);
 
   // Generate animated 🎉 emojis if payment is successful
   useEffect(() => {
-    if (status === "COMPLETED") {
+    if (status === "Completed") {
       let count = 0;
       const interval = setInterval(() => {
         setEmojis((prev) => [
@@ -84,38 +89,40 @@ const Thankyou = () => {
   }, [status]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-main px-10 gap-10 relative overflow-hidden">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-main px-3 gap-10 overflow-hidden relative">
       {isLoading ? (
         <div className="bg-white flex justify-center items-center ringg shadow-lg max-w-md text-center rounded-full p-2 h-48 w-48 relative">
           <div className="text-subMain font-semibold animate-pulse">
             Verifying Payment...
           </div>
         </div>
-      ) : status && status === "COMPLETED" ? (
+      ) : status && status === "Completed" ? (
         <div>
-          <h2 className="text-2xl font-bold text-white text-center">
-            Payment Successful! <br />
-            Thank you for subscribing to our service!
+          <h2 className="text-2xl font-bold text-green text-center">
+            Payment Successful! 🎉 <br />
           </h2>
         </div>
       ) : (
-        <h2 className="text-2xl font-bold text-red-500">Payment Failed!</h2>
+        <h2 className="text-2xl font-bold text-red-500">Payment Failed! 😔</h2>
       )}
 
-      {status === "COMPLETED" ? (
-        <div className=" bg-dry rounded p-8  border-2  border-border flexCol gap-5">
-          <p className="font-semibold text-2xl text-white">
+      {status && status === "Completed" ? (
+        <div className=" bg-dry rounded p-8  border-2  border-border flexCol gap-5 flex flex-col justify-center items-center bg-primary ">
+          <p className="font-semibold text-2xl  text-green">
+            Thank you for your patronage!
+          </p>
+          <p className="font-semibold text-2xl text-green">
             You will be redirected to the home page.
           </p>
-          <p className="font-semibold text-2xl text-white">Happy viewing</p>
-          <p className="font-semibold text-4xl text-white animate-bounce">🤗</p>
+          <p className="font-semibold text-4xl text-green animate-bounce">🤗</p>
         </div>
       ) : (
         ""
       )}
 
       {/* Animated Party Emojis 🎉 */}
-      {status === "COMPLETED" &&
+      {status &&
+        status === "Completed" &&
         emojis.map((emoji) => (
           <span
             key={emoji.id}
@@ -128,6 +135,61 @@ const Thankyou = () => {
             🎉
           </span>
         ))}
+
+      <div
+        id="tableContainer"
+        className="w-full flex flex-col overflow-auto min-h-[300px] gap-3 bg-primary px-4 "
+      >
+        <h2 className="text-2xl font-bold text-green text-center">
+          Products Purchased
+        </h2>
+
+        <table className="table-auto text-gray-500 bg-primary   w-full">
+          <thead className="sticky top-0">
+            <tr className="bg-btn  top-0 left-0">
+              <th className="w-[10%]">S/n</th>
+              <th className="">Style</th>
+              <th className="">Size</th>
+              <th className="">Quantity</th>
+              <th className="">Amount</th>
+              <th className="">Image</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {Products &&
+              Products.map((item, index) => (
+                <tr key={index} className="font-semibold">
+                  <td>{Number(index + 1)}</td>
+                  <td>{item?.style}</td>
+                  <td>{item?.sizee}</td>
+                  <td>{item.quantity}</td>
+                  <td>${item?.price}</td>
+                  <td className="flex justify-center items-center">
+                    <img
+                      src={item?.image}
+                      alt={item?.image}
+                      className="h-[50px] w-[40px] object-cover"
+                    />
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        {Products && Products?.length > 0 ? (
+          <>
+            <div className="flex flex-col gap-1">
+              <p className="text-adminTex text-small italic">
+                Bill: {Bill && Bill}N
+              </p>
+              {/* <p className="text-adminTex text-small italic">Vat: 100N</p> */}
+              {/* <p className="text-adminTex text-small italic">
+                Bill: {Total + 100}N
+              </p> */}
+            </div>
+          </>
+        ) : null}
+      </div>
 
       <style>
         {`
