469f689771b4bf17222222b8379e2e275f77d6b0
Chibuzor <chibuzormekalam@gmail.com>
2025-04-09 19:34:52 +0100

    adding shima

--- src/pages/home/All/Allslides.jsx
+++ src/pages/home/All/Allslides.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from 'react'
 
-import First from "./first";
-import Second from "./second";
-import Third from "./third";
-import Fourth from "./fourth";
-import Fifth from "./fifth";
-import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
+import First from './first'
+import Second from './second'
+import Third from './third'
+import Fourth from './fourth'
+import Fifth from './fifth'
+import { IoIosArrowDropleft, IoIosArrowDropright } from 'react-icons/io'
+import { useSelector } from 'react-redux'
 const Allslides = () => {
+  const cloth = useSelector(state => state?.cloth?.data)
+
   // Declare the array of JSX elements
   const slides = useMemo(
     () => [
@@ -14,109 +17,116 @@ const Allslides = () => {
       <Second key={Second} />,
       <Third key={Third} />,
       <Fourth key={Fourth} />,
-      <Fifth key={Fifth} />,
+      <Fifth key={Fifth} />
     ],
     []
-  );
+  )
 
   // State to track the current slide index
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [isPaused, setIsPaused] = useState(false);
-  const [isMouseOver, setIsMouseOver] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [isPaused, setIsPaused] = useState(false)
+  const [isMouseOver, setIsMouseOver] = useState(false)
 
   // Function to move to the next slide
   const nextSlide = useCallback(() => {
-    setCurrentSlide((prevSlide) =>
+    setCurrentSlide(prevSlide =>
       prevSlide === slides.length - 1 ? 0 : prevSlide + 1
-    );
-  }, [slides]);
+    )
+  }, [slides])
 
   // Function to move to the previous slide
   const prevSlide = useCallback(() => {
-    setCurrentSlide((prevSlide) =>
+    setCurrentSlide(prevSlide =>
       prevSlide === 0 ? slides.length - 1 : prevSlide - 1
-    );
-  }, [slides]);
+    )
+  }, [slides])
 
   // Automatic sliding
   useEffect(() => {
     const intervalId = setInterval(() => {
       if (!isPaused && !isMouseOver) {
-        nextSlide();
+        nextSlide()
       }
-    }, 10000); // Change slide every 10 seconds
-    return () => clearInterval(intervalId); // Cleanup function to clear the interval
-  }, [nextSlide, isPaused, isMouseOver]); // Re-run effect when nextSlide, isPaused, or isMouseOver changes
+    }, 10000) // Change slide every 10 seconds
+    return () => clearInterval(intervalId) // Cleanup function to clear the interval
+  }, [nextSlide, isPaused, isMouseOver]) // Re-run effect when nextSlide, isPaused, or isMouseOver changes
 
   // Event handlers for pause and continue on mouseover and mouseout
   const handleMouseOver = () => {
-    setIsMouseOver(true);
-    setIsPaused(true);
-  };
+    setIsMouseOver(true)
+    setIsPaused(true)
+  }
 
   const handleMouseOut = () => {
-    setIsMouseOver(false);
-    setIsPaused(false);
-  };
+    setIsMouseOver(false)
+    setIsPaused(false)
+  }
 
   return (
-    <div className="w-full md:w-1/2 px-1 md:px-2 overflow-hidden mt-16 md:-mt-10  md:py-16">
-      <h1 className="text-3xl md:py-6 font-sans flex justify-center font-semibold">
+    <div className='w-full md:w-1/2 px-1 md:px-2 overflow-hidden mt-16 md:-mt-10   md:py-16'>
+      <h1 className='text-3xl md:py-6 font-sans flex justify-center font-semibold'>
         Featured
       </h1>
-      <div className="carsel w-full carsel  ">
-        <div
-          className="relative   flex w-full transition ease-in-out duration-1000"
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        >
-          {slides.map((slide, index) => (
+      {cloth.length > 0 ? (
+        <>
+          ;
+          <div className='carsel w-full carsel  '>
             <div
-              key={index}
-              className={`w-full carocel transition ease-in-out duration-1000 bg-slate-400 ${
-                index === currentSlide ? "" : "hidden"
-              } flex justify-center  `}
+              className='relative   flex w-full transition ease-in-out duration-1000'
+              onMouseOver={handleMouseOver}
+              onMouseOut={handleMouseOut}
             >
-              {React.cloneElement(slide, {
-                style: {
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover", // Ensure the image covers the container
-                },
-              })}
+              {slides.map((slide, index) => (
+                <div
+                  key={index}
+                  className={`w-full carocel transition ease-in-out duration-1000 bg-slate-400 ${
+                    index === currentSlide ? '' : 'hidden'
+                  } flex justify-center  `}
+                >
+                  {React.cloneElement(slide, {
+                    style: {
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'cover' // Ensure the image covers the container
+                    }
+                  })}
+                </div>
+              ))}
+              {/* Manual navigation buttons */}
+
+              <div
+                onClick={prevSlide}
+                className='absolute  top-1/2 left-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-full '
+              >
+                <IoIosArrowDropleft className='w-6 h-6' />
+              </div>
+
+              <div
+                onClick={nextSlide}
+                className='absolute top-1/2 right-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-full'
+              >
+                <IoIosArrowDropright className='w-6 h-6 ' />
+              </div>
             </div>
-          ))}
-          {/* Manual navigation buttons */}
-
-          <div
-            onClick={prevSlide}
-            className="absolute  top-1/2 left-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-full "
-          >
-            <IoIosArrowDropleft className="w-6 h-6" />
           </div>
-
-          <div
-            onClick={nextSlide}
-            className="absolute top-1/2 right-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-full"
-          >
-            <IoIosArrowDropright className="w-6 h-6 " />
+          {/* Navigation dots */};
+          <div className='flex justify-center mt-4'>
+            {slides.map((_, index) => (
+              <span
+                key={index}
+                className={`mx-1 h-3 w-3 rounded-full cursor-pointer ${
+                  index === currentSlide ? 'bg-gray-900' : 'bg-gray-400'
+                }`}
+                onClick={() => setCurrentSlide(index)}
+              />
+            ))}
           </div>
-        </div>
-      </div>
-      {/* Navigation dots */}
-      <div className="flex justify-center mt-4">
-        {slides.map((_, index) => (
-          <span
-            key={index}
-            className={`mx-1 h-3 w-3 rounded-full cursor-pointer ${
-              index === currentSlide ? "bg-gray-900" : "bg-gray-400"
-            }`}
-            onClick={() => setCurrentSlide(index)}
-          />
-        ))}
-      </div>
+        </>
+      ) : (
+        <div className=' w-full h-[400px] lg:h-[600px] carsel shimmer'></div>
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default Allslides;
+export default Allslides
--- src/pages/home/ChangeFunction/Categoryy.jsx
+++ src/pages/home/ChangeFunction/Categoryy.jsx
@@ -112,127 +112,138 @@
 
 // export default Category;
 
-import React, { useState, useEffect, useCallback, useMemo } from "react";
-import Ankara from "./Ankara";
-import Ashoebi from "./Ashoebi";
-import Bridals from "./Bridals";
-import Coperate from "./Coperate";
-import Kiddies from "./Kideis";
-import MatchingSet from "./MatchingSet";
-import Kaftan from "./Kaftan";
-import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
+import React, { useState, useEffect, useCallback, useMemo } from 'react'
+import Ankara from './Ankara'
+import Ashoebi from './Ashoebi'
+import Bridals from './Bridals'
+import Coperate from './Coperate'
+import Kiddies from './Kideis'
+import MatchingSet from './MatchingSet'
+import Kaftan from './Kaftan'
+import { IoIosArrowDropleft, IoIosArrowDropright } from 'react-icons/io'
+import { useSelector } from 'react-redux'
 
 const Category = () => {
+  const cloth = useSelector(state => state?.cloth?.data)
+  console.log('clothCat', cloth)
+
   // Declare the array of JSX elements
   const slides = useMemo(
     () => [
-      <Ankara key="Ankara" />,
-      <Ashoebi key="Ashoebi" />,
-      <Coperate key="Coperate" />,
-      <Kaftan key="Kaftan" />,
-      <Bridals key="Bridals" />,
-      <MatchingSet key="MatchingSet" />,
-      <Kiddies key="Kiddies" />,
+      <Ankara key='Ankara' />,
+      <Ashoebi key='Ashoebi' />,
+      <Coperate key='Coperate' />,
+      <Kaftan key='Kaftan' />,
+      <Bridals key='Bridals' />,
+      <MatchingSet key='MatchingSet' />,
+      <Kiddies key='Kiddies' />
     ],
     []
-  );
+  )
 
   // State to track the current slide index
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [isPaused, setIsPaused] = useState(false);
-  const [isMouseOver, setIsMouseOver] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [isPaused, setIsPaused] = useState(false)
+  const [isMouseOver, setIsMouseOver] = useState(false)
 
   // Function to move to the next slide
   const nextSlide = useCallback(() => {
-    setCurrentSlide((prevSlide) =>
+    setCurrentSlide(prevSlide =>
       prevSlide === slides.length - 1 ? 0 : prevSlide + 1
-    );
-  }, [slides]);
+    )
+  }, [slides])
 
   // Function to move to the previous slide
   const prevSlide = useCallback(() => {
-    setCurrentSlide((prevSlide) =>
+    setCurrentSlide(prevSlide =>
       prevSlide === 0 ? slides.length - 1 : prevSlide - 1
-    );
-  }, [slides]);
+    )
+  }, [slides])
 
   // Automatic sliding
   useEffect(() => {
     const intervalId = setInterval(() => {
       if (!isPaused && !isMouseOver) {
-        nextSlide();
+        nextSlide()
       }
-    }, 30000); // Change slide every 10 seconds
-    return () => clearInterval(intervalId); // Cleanup function to clear the interval
-  }, [nextSlide, isPaused, isMouseOver]); // Re-run effect when nextSlide, isPaused, or isMouseOver changes
+    }, 30000) // Change slide every 10 seconds
+    return () => clearInterval(intervalId) // Cleanup function to clear the interval
+  }, [nextSlide, isPaused, isMouseOver]) // Re-run effect when nextSlide, isPaused, or isMouseOver changes
 
   // Event handlers for pause and continue on mouseover and mouseout
   const handleMouseOver = () => {
-    setIsMouseOver(true);
-    setIsPaused(true);
-  };
+    setIsMouseOver(true)
+    setIsPaused(true)
+  }
 
   const handleMouseOut = () => {
-    setIsMouseOver(false);
-    setIsPaused(false);
-  };
+    setIsMouseOver(false)
+    setIsPaused(false)
+  }
 
   return (
-    <div className="w-full px-1 md:px-2 md:w-1/2 overflow-hidden md:m-0 mt-10 md:mt-5 md:py-16">
-      <h1 className="text-3xl md:py-6 font-sans flex justify-center font-semibold">
+    <div className='w-full px-1 md:px-2 md:w-1/2 overflow-hidden md:m-0 mt-10 md:mt-5  md:py-16'>
+      <h1 className='text-3xl md:py-6 font-sans flex justify-center font-semibold'>
         Category
       </h1>
-      <div className="carsel w-full">
-        <div
-          className="relative flex w-full   transition ease-in-out duration-1000"
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        >
-          {slides.map((slide, index) => (
-            <div
-              key={index}
-              className={`w-full carocel transition  ease-in-out duration-1000 bg-slate-400 ${
-                index === currentSlide ? "" : "hidden"
-              } flex justify-center   `}
-            >
-              {React.cloneElement(slide, {
-                style: {
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover", // Ensure the image covers the container
-                },
-              })}
-            </div>
-          ))}
-          {/* Manual navigation buttons */}
-          <div
-            onClick={prevSlide}
-            className="absolute  top-1/2 left-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-full "
-          >
-            <IoIosArrowDropleft className="w-6 h-6 " />
-          </div>
-          <div
-            onClick={nextSlide}
-            className="absolute top-1/2 right-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-full"
-          >
-            <IoIosArrowDropright className="w-6 h-6 " />
-          </div>
-        </div>
-      </div>
-      {/* Navigation dots */}
-      <div className="flex justify-center mt-4">
-        {slides.map((_, index) => (
-          <span
-            key={index}
-            className={`mx-1 h-3 w-3 rounded-full cursor-pointer ${
-              index === currentSlide ? "bg-gray-900" : "bg-gray-400"
-            }`}
-            onClick={() => setCurrentSlide(index)}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
 
-export default Category;
+      {cloth.length > 0 ? (
+        <>
+          <div className='carsel w-full'>
+            <div
+              className='relative flex w-full   transition ease-in-out duration-1000'
+              onMouseOver={handleMouseOver}
+              onMouseOut={handleMouseOut}
+            >
+              {slides.map((slide, index) => (
+                <div
+                  key={index}
+                  className={`w-full carocel transition  ease-in-out duration-1000 bg-slate-400 ${
+                    index === currentSlide ? '' : 'hidden'
+                  } flex justify-center   `}
+                >
+                  {React.cloneElement(slide, {
+                    style: {
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'cover' // Ensure the image covers the container
+                    }
+                  })}
+                </div>
+              ))}
+              {/* Manual navigation buttons */}
+              <div
+                onClick={prevSlide}
+                className='absolute  top-1/2 left-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-full '
+              >
+                <IoIosArrowDropleft className='w-6 h-6 ' />
+              </div>
+              <div
+                onClick={nextSlide}
+                className='absolute top-1/2 right-3 transform -translate-y-1/2 px-2 py-1 carocel-btn rounded-full'
+              >
+                <IoIosArrowDropright className='w-6 h-6 ' />
+              </div>
+            </div>
+          </div>
+          {/* Navigation dots */};
+          <div className='flex justify-center mt-4'>
+            {slides.map((_, index) => (
+              <span
+                key={index}
+                className={`mx-1 h-3 w-3 rounded-full cursor-pointer ${
+                  index === currentSlide ? 'bg-gray-900' : 'bg-gray-400'
+                }`}
+                onClick={() => setCurrentSlide(index)}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className=' w-full h-[400px] lg:h-[600px] carsel shimmer'></div>
+      )}
+    </div>
+  )
+}
+
+export default Category
